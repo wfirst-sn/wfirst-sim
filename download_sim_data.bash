56745ea1131a653dd13b43dfb4744a93063b78a3
David Rabinowitz <david.rabinowitz@yale.edu>
2016-12-07 13:51:16 -0800

    added download_sim_data.bash and modified install.bash to export WFIRST-SIM-DATA path

---
+++ download_sim_data.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+# download_sim_data.bash
+#
+# download data files needed by wfirst-sim scripts
+
+SOURCE_PATH="https://github.com/wfirst-sn/wfirst-sim-data "
+WFIRST_SIM_DATA="wfirst-sim-data"
+ 
+d0=`pwd`
+
+# get the concorde source
+git clone $SOURCE_PATH
+
+if  [ ! -d $WFIRST_SIM_DATA ]; then
+  echo "ERROR : could not download directory $WFIRST_SIM_DATA from $SOURCE_PATH"
+  exit 1
+fi
+
+echo "$WFIRST_SIM_DATA successfully downloaded "
+
